96c9b136e1e9eeacbf561623dd8ac2ba53656dc6
Hope Dorkenoo <compounddork@gmail.com>
2023-02-24 08:50:06 +0000

    add env vars and config

--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 
 	"github.com/ghost-codes/simplebank/api"
 	db "github.com/ghost-codes/simplebank/db/sqlc"
+	"github.com/ghost-codes/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:password@localhost:5432/bank?sslmode=disable"
-	addr     = "0.0.0.0:3000"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("unable to load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(addr)
+	err = server.Start(config.ServerAddr)
 	if err != nil {
 		log.Fatal("error occured, Server could not start; Error:", err)
 	}
