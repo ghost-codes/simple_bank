da65854b0cb19da9ed1150fa8e5d043fb7a28683
Hope Dorkenoo <compounddork@gmail.com>
2023-03-08 20:54:01 +0000

    fix url issue

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ set -e
 
 echo "run db migration"
 echo "=====> $DB_SOURCE"
-/app/migrate -path /app/migration -database "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable
-" -verbose up
+/app/migrate -path /app/migration -database "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable" -verbose up
 
 echo "start up"
 exec "$@"
